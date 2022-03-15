2b69f815f300c5cc5c4c69c6ba7e1d85885ff762
daishiman <daishimanju@gmail.com>
2022-03-15 21:13:33 +0900

    06. レンダリング確認用のコーディング

--- セクション2-03/section2-3/src/App.jsx
+++ セクション2-03/section2-3/src/App.jsx
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import { useState } from "react";
+import { ChildArea } from './ChildArea';
 import './App.css';
 
-export default function App() {
-  const [count, setCount] = useState(0);
 
-  const onClickCountUp = () => {
-    console.log("App");
-    setCount(count + 1);
-  };
+export default function App() {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
-      <h1>hello React</h1>
-      <h2>start React</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText}/>
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
     </div>
   );
 }
