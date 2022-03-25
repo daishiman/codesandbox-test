33d908c23d379f805222fefe506b7e1a7d294bac
daishiman <daishimanju@gmail.com>
2022-03-25 22:43:37 +0900

    24. stateを渡すページ遷移

--- セクション2-05/section2-5/src/Page1DetailA.jsx
+++ セクション2-05/section2-5/src/Page1DetailA.jsx
@@ -4,8 +4,8 @@ export const Page1DetailA = () => {
 	const { state } = useLocation();
 	console.log(state);
 	return (
-	  <div>
-	    <h1>Page1DetailAページです</h1>
-	  </div>
+		<div>
+			<h1>Page1DetailAページです</h1>
+		</div>
 	)
-      }
+}
