3d459d67b83722044bc9fed66af308bc69da84d9
daishiman <daishimanju@gmail.com>
2022-03-26 20:48:15 +0900

    37. Contextでのstate管理(基本的な使い方)

--- セクション2-06/section2-6/src/App.js
+++ セクション2-06/section2-6/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Router } from './router/Router';
+import { UserProvider } from './providers/UserProvider';
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />;
+    </UserProvider>
+  );
 }
