15b0eec7c56915138e600e9e6caec4e3dce17af0
daishiman <daishimanju@gmail.com>
2022-03-26 23:14:35 +0900

    39. Contextでのstate管理(再レンダリングの最適化)

--- セクション2-06/section2-6/src/components/atoms/button/SecondaryButton.jsx
+++ セクション2-06/section2-6/src/components/atoms/button/SecondaryButton.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { BaseButton } from "./BaseButton";
+import { BaseButton } from './BaseButton';
 
 export const SecondaryButton = (props) => {
-	const { children, onClick } = props;
+  const { children, onClick } = props;
   return <SButton onClick={onClick}>{children}</SButton>;
-}
+};
 
 const SButton = styled(BaseButton)`
-	background-color: #11999e;
-`
+  background-color: #11999e;
+`;
