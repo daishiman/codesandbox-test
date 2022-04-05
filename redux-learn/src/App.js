be0637937f38589db462aaadd627391d46c1b674
daishiman <daishimanju@gmail.com>
2022-04-05 21:59:14 +0900

    他のコンポーネントからのアクセス

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import Count from './components/count';
 
 function App({ count }) {
   return (
     <div className="App">
       <h1>Redux Learn</h1>
       <p>Count:{count}</p>
+      <Count />
     </div>
   );
 }
