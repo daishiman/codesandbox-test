71de7402655547cc5c96f2aa7631f6d1f266a543
daishiman <daishimanju@gmail.com>
2022-04-06 15:16:03 +0900

    非同期の処理

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Count from './components/count';
+import { Post } from './components/Post';
 
 function App() {
   return (
     <div className="App">
       <h1>Redux Learn</h1>
       <Count />
+      <Post />
     </div>
   );
 }
