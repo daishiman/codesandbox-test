473300412a62e4309652274c25cb69759ece7f37
daishiman <daishimanju@gmail.com>
2022-04-06 14:48:43 +0900

    useDispatch Hooksの利用

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './App.css';
+import Count from './components/count';
 
-function App({ count, increase, decrease }) {
+function App() {
   return (
     <div className="App">
       <h1>Redux Learn</h1>
-      <p>Count:{count}</p>
-      <button onClick={increase}>Up</button>
-      <br />
-      <button onClick={decrease}>Down</button>
+      <Count />
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.count,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increase: () => dispatch({ type: 'INCREASE_COUNT' }),
-    decrease: () => dispatch({ type: 'DECREASE_COUNT' }),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
